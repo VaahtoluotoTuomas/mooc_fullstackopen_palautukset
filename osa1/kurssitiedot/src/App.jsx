270852d181a99da617dd00413832353b1b46bb5d
VaahtoluotoTuomas <tuomas.vaahtoluoto@gmail.com>
2025-11-06 10:15:44 +0200

    1.1 ja 1.2 valmiit

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,34 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* Refaktoroi vielä komponentti Content siten, että se ei itse 
+renderöi yhdenkään osan nimeä eikä sen tehtävälukumäärää vaan 
+ainoastaan kolme Part-nimistä komponenttia, joista kukin siis 
+renderöi yhden osan nimen ja tehtävämäärän.*/
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
+  console.log(props)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part1 = (props) => {
+    console.log(props)
+  return (
+    <div>
+      {props.part1} {props.exercises1}
+    </div>
+  )
+}
+
+const Part2 = (props) => {
+    console.log(props)
+  return (
+    <div>
+      {props.part2} {props.exercises2}
+    </div>
+  )
+}
+
+const Part3 = (props) => {
+    console.log(props)
+  return (
+    <div>
+      {props.part3} {props.exercises3}
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part1 part1={props.part1} exercises1={props.exercises1} />
+      <Part2 part2={props.part2} exercises2={props.exercises2} />
+      <Part3 part3={props.part3} exercises3={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+      <p>Number of excercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+return (
+    <div>
+      <Header course={course} />
+      <Content 
+        part1={part1} part2={part2} part3={part3}
+        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
+        />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+
+    </div>
   )
 }
 
